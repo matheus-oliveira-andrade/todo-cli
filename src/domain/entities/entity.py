3708d395896cef9a9193c481abc1d42583cf4000
matheus-oliveira-andrade <mathnhais@outlook.com>
2023-06-05 20:55:28 -0300

    refactor: id to todo_id

--- src/domain/entities/entity.py
+++ src/domain/entities/entity.py
@@ -2,7 +2,7 @@ from abc import ABC
 
 
 class Entity(ABC):
-    def __init__(self, id, modified_at, created_at):
-        self.id = id;
-        self.modified_at = modified_at;
+    def __init__(self, todo_id, modified_at, created_at):
+        self.todo_id = todo_id
+        self.modified_at = modified_at
         self.created_at = created_at
--- src/domain/entities/todo.py
+++ src/domain/entities/todo.py
@@ -4,14 +4,24 @@ from src.domain.enums.todo_status import TodoStatus
 
 class Todo(Entity):
 
-    def __init__(self, title, description, status, tags):
+    def __init__(self, title, description, status, tags, todo_id, created_at, modified_at):
+        super().__init__(todo_id, created_at, modified_at)
+
         self.title = title
         self.description = description
         self.status = status
         self.tags = tags
 
     def mark_as_done(self) -> None:
+        self.modified_at = datetime.utcnow().time()
         self.status = TodoStatus.DONE
 
     def is_done(self) -> bool:
         return self.status == TodoStatus.DONE
+
+    @staticmethod
+    def create_new(title, description, tags) -> 'Todo':
+        now = datetime.utcnow()
+        todo_id = str(uuid.uuid4())
+
+        return Todo(title, description, TodoStatus.PENDING, tags, todo_id, now, now)
