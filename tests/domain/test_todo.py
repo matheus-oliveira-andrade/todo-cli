fbc97d1a8ac0e12ea0a6c791ece7afe649e5a5d3
matheus-oliveira-andrade <mathnhais@outlook.com>
2023-06-13 19:46:35 -0300

    refactor: tests todo

--- tests/domain/test_todo.py
+++ tests/domain/test_todo.py
@@ -1,17 +1,19 @@
+import unittest
+
 from src.domain.todo import Todo
 from src.domain.todo import TodoStatus
 
 
-class TestTodo:
+class TestTodo(unittest.TestCase):
     def test_create_new_should_return_new_todo(self):
         # arrange & act
         todo = Todo.create_new('todo', 'todo description', ['tag1', 'tag2'])
 
         # assert
-        assert todo is not None
-        assert todo.title == 'todo'
-        assert todo.description == 'todo description'
-        assert len(todo.tags) == 2
+        self.assertIsNotNone(todo)
+        self.assertEqual('todo', todo.title)
+        self.assertEqual('todo description', todo.description)
+        self.assertEqual(2, len(todo.tags))
 
     def test_mark_as_done_should_correctly_update_status(self):
         # arrange
@@ -21,7 +23,7 @@ class TestTodo:
         todo.mark_as_done()
 
         # assert
-        assert todo.status == TodoStatus.DONE
+        self.assertEqual(TodoStatus.DONE, todo.status)
 
     def test_is_done_should_return_true_if_status_is_done(self):
         # arrange
@@ -31,4 +33,4 @@ class TestTodo:
         result = todo.is_done()
 
         # assert
-        assert result is False
+        self.assertFalse(result)
