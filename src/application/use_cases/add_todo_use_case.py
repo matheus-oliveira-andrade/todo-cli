b418d9141b7ceaa170dd5ea4910a21fbb3c621e3
matheus-oliveira-andrade <mathnhais@outlook.com>
2023-06-13 20:01:02 -0300

    feat: add logger in add todo use case

--- src/application/use_cases/add_todo_use_case.py
+++ src/application/use_cases/add_todo_use_case.py
@@ -1,12 +1,16 @@
+import logging
+
 from src.domain.todo import Todo
 
 
 class AddTodoUseCase:
 
     def __init__(self, todo_repository):
+        self.logger = logging.getLogger()
         self.todo_repository = todo_repository
 
     def handle(self, add_todo_dto) -> bool:
+        self.logger.info(f"adding todo {add_todo_dto.title}")
 
         todo = Todo.create_new(add_todo_dto.title, add_todo_dto.description, add_todo_dto.tags)
 
