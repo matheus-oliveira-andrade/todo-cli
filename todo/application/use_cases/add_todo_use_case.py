157e66c3cbfb82a0b2ee0b36228a612df7dfcbd7
matheus-oliveira-andrade <mathnhais@outlook.com>
2023-06-22 20:02:07 -0300

    refactor: add todo typing todo repository and logger name

--- todo/application/use_cases/add_todo_use_case.py
+++ todo/application/use_cases/add_todo_use_case.py
@@ -2,12 +2,13 @@ import logging
 
 from todo.domain.todo import Todo
 from ..dtos.add_todo_dto import AddTodoDto
+from ..repositories.todo_repository import TodoRepository
 
 
 class AddTodoUseCase:
 
-    def __init__(self, todo_repository):
-        self.logger = logging.getLogger()
+    def __init__(self, todo_repository: TodoRepository) -> None:
+        self.logger = logging.getLogger('AddTodoUseCase')
         self.todo_repository = todo_repository
 
     def handle(self, add_todo_dto: AddTodoDto) -> bool:
