ac51d818251c89f27e2df7ad3d3e527943ef76b0
matheus-oliveira-andrade <mathnhais@outlook.com>
2023-06-23 18:45:41 -0300

    fix: trailing comma causing interpreted as a tuple

--- todo/application/dtos/add_todo_dto.py
+++ todo/application/dtos/add_todo_dto.py
@@ -1,6 +1,6 @@
 class AddTodoDto:
     def __init__(self, title: str, description: str, tags: list[str]):
-        self.title = title
-        self.description = description,
-        self.tags = tags
+        self.title: str = title
+        self.description: str = description
+        self.tags: list[str] = tags
 
